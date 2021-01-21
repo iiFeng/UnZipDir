0e896208632f1e319164f099a0537239eafee823
iiFeng <iamifenghuang@gmail.com>
2021-01-21 22:04:57 +0800

    add unzip method

--- src/com/company/UnZipDir.java
+++ src/com/company/UnZipDir.java
@@ -4,18 +4,18 @@ import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-public class Main {
+public class UnZipDir {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("add file path");
+        if (args.length != 2) {
+            System.err.println("add file path and directory");
             System.exit(1);
         }
-        unzip(args[0],args[1]);
+        unzip(args[0], args[1]);
     }
 
     //传入一个压缩文件或目录
-    public static void unzip(String filePath,String outputDir) {
+    public static void unzip(String filePath, String outputDir) {
         //压缩文件则创建文件夹
         if (filePath.matches("[.zip]")) {
             File dir = new File(outputDir);
@@ -32,19 +32,23 @@ public class Main {
             ZipInputStream zip = new ZipInputStream(fis);
             //如果是压缩文件
             ZipEntry entry = zip.getNextEntry();
+            boolean flag = true;
             while (entry != null) {
                 String fileName = entry.getName();
                 File newFile = new File(outputDir + File.separator + fileName);
-                System.out.println("Unzipping to " + newFile.getAbsolutePath());
-                //create directories for sub directories in zip
-                new File(newFile.getParent()).mkdirs();
-                System.out.println("mkdir " + newFile.getAbsolutePath());
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zip.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+                if (flag || newFile.isDirectory()) {
+                    flag = false;
+                    System.out.println("Unzipping to " + newFile.getAbsolutePath());
+                    //create directories for sub directories in zip
+                    new File(newFile.getParent()).mkdirs();
+                    FileOutputStream fos = new FileOutputStream(newFile);
+                    int len;
+                    while ((len = zip.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                    fos.close();
                 }
-                fos.close();
+
                 //close this ZipEntry
                 zip.closeEntry();
                 entry = zip.getNextEntry();
@@ -53,7 +57,8 @@ public class Main {
             zip.closeEntry();
             zip.close();
             fis.close();
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
